af602c8e96528eead8770376be997684fa6e80b4
maxobi243 <maxq338@gmail.com>
2026-02-04 13:26:12 +1100

    Update server.ps1

--- server.ps1
+++ server.ps1
@@ -51,8 +51,14 @@ while ($true) {
 
         $readAmmt = $stream.Read($passBuf, 0, $passBuf.Length)
         $sendPass = [Text.Encoding]::ASCII.GetString($passBuf, 0, $readAmmt)
+        $plaintext = "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000194f8e5ed645714e8a9a0ee176f9085800000000020000000000106600000001000020000000ff0a58066975f1027a0f0ac15fa83959e158404d4a1b6488c52d7ceea714a87f000000000e80000000020000200000000f9ce8f59c81ddac974f762d0a73fa0f278aa3d240279f9ab21974c7354f7af520000000dfdbf29e86ff8d76b4ce9c273736b71a38315f48bc1d1df8dfe666e92a0d100e40000000e614212e3663671b3059342b9e1c0c619cd1aaedd0d0fe1c15743e958373a9842d430a5bee8d29a95d490df68af568b9b4896a9dacce83aacb641e4f37c1f724"
+        $secure =ConvertTo-SecureString $plaintext
+        $plain = [Runtime.InteropServices.Marshal]::PtrToStringAuto(
+            [Runtime.InteropServices.Marshal]::SecureStringToBSTR($secure)
+        )
 
-        if ($sendPass -ne "shellreverse") {
+
+        if ($sendPass -ne $plain) {
             Send "Wrong Pass LEAVE"
             exit
         }
